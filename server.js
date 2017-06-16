a5eeb485dd76d9057239b30d97cca4937a6525a3
dhrubesh <dhrubesh97@gmail.com>
2017-06-16 19:25:03 +0000

    basic set-up

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
-const app = express()
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
