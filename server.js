21502276a5554cd04c7640141a1af1f6fc64d207
dhrubesh <dhrubesh97@gmail.com>
2017-06-17 05:04:06 +0000

    folder structure update

--- server.js
+++ server.js
@@ -12,10 +12,72 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+app.set('views',path.join(__dirname + 'views'))
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport initialize
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Express Validator form github
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+
+// Global Variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg'); //success
+  res.locals.error_msg = req.flash('error_msg'); //error
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+//middleware for routes
+app.use('/', routes);
+app.use('/users', users);
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
 
 app.listen(8080, function () {
   console.log(' app listening on port 8080...')
