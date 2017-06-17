069d88d56e78920bd3d9ffeabf189eabb1a9c10c
dhrubesh <dhrubesh97@gmail.com>
2017-06-17 20:42:58 +0000

    schema set up

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,38 @@ router.get('/login', function(req, res){
 	res.render('login');
 });
 
+//resgister the user
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+	// console.log(name)
+	
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+	
+	var errors = req.validationErrors();
 
+	if(errors){
+		// console.log(errors)
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		// console.log("passed")
+		var newUser = new User({
+			name: name,
+			email:email,
+			username: username,
+			password: password
+		});
+		}
+});
 
 module.exports = router;
